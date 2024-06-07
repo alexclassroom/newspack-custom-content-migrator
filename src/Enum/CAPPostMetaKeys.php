145fed103deaa235eb5b50461796c60424a2ce5e
Eddie Carrasco <eddies.code.shop@gmail.com>
2024-06-07 15:01:07 -0400

    feat: new Enum file to house CAP related post meta key

---
+++ src/Enum/CAPPostMetaKeys.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NewspackCustomContentMigrator\Enum;
+
+enum CAPPostMetaKeys: string {
+	case LOGIN        = 'cap-user_login';
+	case EMAIL        = 'cap-user_email';
+	case FIRST_NAME   = 'cap-first_name';
+	case LAST_NAME    = 'cap-last_name';
+	case DISPLAY_NAME = 'cap-display_name';
+	case DESCRIPTION  = 'cap-description';
+}
