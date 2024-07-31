fe0852a521b1f22ae5bf0264f5f4f83fa6730fd7
Eddie Carrasco <eddies.code.shop@gmail.com>
2024-07-31 17:12:39 -0400

    adding cap-linked_account

--- src/Enum/CAPPostMetaKeys.php
+++ src/Enum/CAPPostMetaKeys.php
@@ -3,10 +3,11 @@
 namespace NewspackCustomContentMigrator\Enum;
 
 enum CAPPostMetaKeys: string {
-	case LOGIN        = 'cap-user_login';
-	case EMAIL        = 'cap-user_email';
-	case FIRST_NAME   = 'cap-first_name';
-	case LAST_NAME    = 'cap-last_name';
-	case DISPLAY_NAME = 'cap-display_name';
-	case DESCRIPTION  = 'cap-description';
+	case LOGIN          = 'cap-user_login';
+	case EMAIL          = 'cap-user_email';
+	case FIRST_NAME     = 'cap-first_name';
+	case LAST_NAME      = 'cap-last_name';
+	case DISPLAY_NAME   = 'cap-display_name';
+	case DESCRIPTION    = 'cap-description';
+	case LINKED_ACCOUNT = 'cap-linked_account';
 }
