b6dae92c48c4343b6f21ac71766bfca44fa12a76
Ivan Uravic <iuravic@gmail.com>
2024-12-31 11:07:42 +0100

    One more space in error message for perfection :)

--- src/Logic/ContentDiffMigrator.php
+++ src/Logic/ContentDiffMigrator.php
@@ -1102,7 +1102,7 @@ class ContentDiffMigrator {
 					
 					if ( is_wp_error( $term_insert_result ) ) {
 						$error_messages[] = sprintf(
-							"Warning, could not insert term='%s' taxonomy='%s' live_term_id=%s for live_post_ID=%s . This is totally OK if you did not wish to migrate this term taxonomy. Message :%s",
+							"Warning, could not insert term='%s' taxonomy='%s' live_term_id=%s for live_post_ID=%s . This is totally OK if you did not wish to migrate this term taxonomy. Message: %s",
 							$live_term_name,
 							$live_term_taxonomy_row['taxonomy'],
 							$live_term_id,
