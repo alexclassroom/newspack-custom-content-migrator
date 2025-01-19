c9702f232a346cee4b7427da01f0033707b447ee
ronchambers <ronchambers@gmail.com>
2025-01-19 11:27:43 -0800

    Add America Mag: refactor

--- src/Command/PublisherSpecific/AmericaMagMigrator.php
+++ src/Command/PublisherSpecific/AmericaMagMigrator.php
@@ -14,14 +14,6 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 
 	private string $migration_name = 'am_mag';
 
-	private function __construct() {
-		add_filter( 'fgd2wp_map_taxonomy', [ $this, 'fg_filter_map_taxonomy' ], 11, 3 );
-		add_filter( 'fgd2wp_get_node_types', [ $this, 'fg_filter_get_node_types' ], 11, 1 );
-		add_filter( 'fgd2wp_pre_register_post_type', [ $this, 'fg_filter_fgd2wp_pre_register_post_type' ], 11, 3 );
-
-		add_filter( 'fgd2wpp_get_users_sql', [ $this, 'filter_fgd2wpp_get_users_sql' ], 10, 2 );
-	}
-
 	public static function register_commands(): void {
 		WP_CLI::add_command(
 			'newspack-content-migrator am-mag-import',
@@ -33,14 +25,33 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 	 * Run the import.
 	 */
 	public function cmd_run_import( array $pos_args, array $assoc_args ): void {
-		DrupalMigrator::run_migration( 'am_mag' );
+		
+		// Setup FG plugin's filters.
+		add_filter( 'fgd2wp_get_node_types',         [ $this, 'fgd2wp_get_node_types' ], 11, 1 );
+		add_filter( 'fgd2wp_map_taxonomy',           [ $this, 'fgd2wp_map_taxonomy' ], 11, 3 );
+		add_filter( 'fgd2wp_pre_register_post_type', [ $this, 'fgd2wp_pre_register_post_type' ], 11, 3 );
+		add_filter( 'fgd2wpp_get_users_sql',         [ $this, 'fgd2wpp_get_users_sql' ], 10, 2 );
+
+		// Call NMT's migrator using a unique migration name.
+		DrupalMigrator::cmd_wrap_drupal_import( [ 'am_mag' ], [] );
+
 	}
 
-	public function add_hooks(): void {
-		add_filter( 'fgd2wp_get_node_types', [ $this, 'fg_filter_get_node_types' ], 11, 1 );
+	/**
+	 * HOOKS
+	 */
+
+	 public function fgd2wp_get_node_types( array $node_types ): array {
+		$types_to_migrate = [
+			'article',
+			'book_review',
+			// .. add more node types here.
+		];
+
+		return array_filter( $node_types, fn( $type ) => in_array( $type, $types_to_migrate ), ARRAY_FILTER_USE_KEY );
 	}
 
-	public function fg_filter_map_taxonomy( $wp_taxonomy, $taxonomy ) {
+	public function fgd2wp_map_taxonomy( $wp_taxonomy, $taxonomy ) {
 		switch ( strtolower( $taxonomy ) ) {
 			case 'topics':
 				$wp_taxonomy = 'post_tag';
@@ -53,17 +64,7 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 		return $wp_taxonomy;
 	}
 
-	public function fg_filter_get_node_types( array $node_types ): array {
-		$types_to_migrate = [
-			'article',
-			'book_review',
-			// .. add more node types here.
-		];
-
-		return array_filter( $node_types, fn( $type ) => in_array( $type, $types_to_migrate ), ARRAY_FILTER_USE_KEY );
-	}
-
-	public function fg_filter_fgd2wp_pre_register_post_type( $post_type, $node_type ) {
+	public function fgd2wp_pre_register_post_type( $post_type, $node_type ) {
 		// Map to post.
 		if ( 'book_review' === $node_type ) {
 			$post_type = 'post';
@@ -72,24 +73,23 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 		return $post_type;
 	}
 
-	public function filter_fgd2wpp_get_users_sql( string $sql ): string {
-		$limit        = 10; // Pretty random, but you get the idea
+	public function fgd2wpp_get_users_sql( string $sql ): string {
+		
+		$limit        = 10; // Possibly used to "batch" x number at a time?
 		$last_user_id = (int) get_option( 'fgd2wp_last_user_id' ); // to restore the import where it left
 		$prefix       = DrupalHelper::get_tables_prefix();
 
 		$sql = "
-					SELECT u.uid, u.name, u.mail, u.pass, u.created, up.user_picture_target_id AS picture
-					FROM {$prefix}users_field_data u
-					LEFT JOIN {$prefix}user__user_picture up ON up.entity_id = u.uid
-					JOIN user__roles ur ON u.uid = ur.entity_id 
-
-				WHERE
-				    ur.roles_target_id IN ('editor', 'web_editor')
-				AND u.uid > '$last_user_id'
-				AND u.status = 1
-				ORDER BY u.uid
-				LIMIT $limit
-			";
+			SELECT u.uid, u.name, u.mail, u.pass, u.created, up.user_picture_target_id AS picture
+			FROM {$prefix}users_field_data u
+			LEFT JOIN {$prefix}user__user_picture up ON up.entity_id = u.uid
+			JOIN user__roles ur ON u.uid = ur.entity_id 
+			WHERE ur.roles_target_id IN ('editor', 'web_editor')
+			AND u.uid > '$last_user_id'
+			AND u.status = 1
+			ORDER BY u.uid
+			LIMIT $limit
+		";
 
 		return $sql;
 	}
