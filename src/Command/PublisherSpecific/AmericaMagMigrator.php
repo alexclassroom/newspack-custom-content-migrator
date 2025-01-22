90a46f7b7eaa6e87680b3a249898295931bd94b7
ronchambers <ronchambers@gmail.com>
2025-01-22 15:56:24 -0800

    Add America Mag: option for only_authors

--- src/Command/PublisherSpecific/AmericaMagMigrator.php
+++ src/Command/PublisherSpecific/AmericaMagMigrator.php
@@ -28,9 +28,9 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 		
 		// Setup FG plugin's filters.
 		add_filter( 'fgd2wp_get_node_types',         [ $this, 'fgd2wp_get_node_types' ], 11, 1 );
+		// add_filter( 'fgd2wp_get_nodes_sql',          [ $this, 'fgd2wp_get_nodes_sql' ], 10, 6 );
 		add_filter( 'fgd2wp_map_taxonomy',           [ $this, 'fgd2wp_map_taxonomy' ], 11, 3 );
 		add_filter( 'fgd2wp_pre_register_post_type', [ $this, 'fgd2wp_pre_register_post_type' ], 11, 3 );
-		add_filter( 'fgd2wpp_get_users_sql',         [ $this, 'fgd2wpp_get_users_sql' ], 10, 2 );
 
 		// global $fgd2wpp;
 		// var_dump( $fgd2wpp );
@@ -41,39 +41,33 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 		// FYI: unset($wp_filter['wp_insert_post']); // Remove the "wp_insert_post" that consumes a lot of CPU and memory
 		
 		// Logging.
-		add_action( 'fgd2wp_post_import_post', function( $new_post_id, $node, $content_type, $post_type, $entity_type ) {
-			WP_CLI::line( 'fgd2wp_post_import_post (AFTER): ' . json_encode( array( 
-				$new_post_id, $node, $content_type, $post_type, $entity_type,
-			) ) );
-		}, 10, 5 );
+		// add_action( 'fgd2wp_post_import_post', function( $new_post_id, $node, $content_type, $post_type, $entity_type ) {
+		// 	WP_CLI::line( 'fgd2wp_post_import_post (AFTER): ' . json_encode( array( 
+		// 		$new_post_id, $node, $content_type, $post_type, $entity_type,
+		// 	) ) );
+		// }, 10, 5 );
 
-
-
-
-		// Order descending:
-		add_filter( 'fgd2wp_get_nodes_sql', function( $sql, $prefix, $last_drupal_id, $limit, $content_type, $entity_type ) {
-	
-			// $sql = "
-			// 	AND n.nid > '$last_drupal_id'
-			// 	ORDER BY n.nid
-			// 	LIMIT $limit
-			// ";
-			
-			// where clause.  the first time $last_drupal_id will be (int) 0 . this will cause the MAX ID to be on the top of DESC.
-			// upon each insert, $last_drupal_id will progressively get less.  so need to change to AND n.nid < '$last_drupal_id'
-			if( $last_drupal_id > 0 ) {
-				$sql = str_replace( 'AND n.nid > ', 'AND n.nid < ', $sql );
-			}
-
-			// order by.
-			$sql = str_replace( 'ORDER BY n.nid', 'ORDER BY n.nid DESC', $sql );
-
-			return $sql;
-
-		}, 10, 6 );
-
-
-
+		// Premium options / Default values / FG plugin version 3.85.2
+		// $this->premium_options = array(
+		// 	'cpt_format'				=> 'acf',
+		// 	'unicode_usernames'			=> false,
+		// 	'links'						=> 'as_links',
+		// 	'url_redirect'				=> true,
+		// 	'skip_taxonomies'			=> false,
+		// 	'skip_nodes'				=> false,
+		// 	'nodes_to_skip'				=> array(),
+		// 	'skip_users'				=> false,
+		// 	'only_authors'				=> false,
+		// 	'skip_menus'				=> false,
+		// 	'skip_comments'				=> false,
+		// 	'skip_blocks'				=> false,
+		// 	'skip_redirects'			=> false,
+		// );
+		// had filter not existed, db option name is get_option('fgd2wpp_options') * note the extra "p" for premium
+		add_filter( 'fgd2wpp_post_init_premium_options', function( $premium_options ) {
+			$premium_options['only_authors'] = true;
+			return $premium_options;
+		}, 10, 1);	
 
 		// Call NMT's migrator using a unique migration name.
 		DrupalMigrator::cmd_wrap_drupal_import( [ 'am_mag' ], [] );
@@ -87,11 +81,28 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 	 public function fgd2wp_get_node_types( array $node_types ): array {
 		$types_to_migrate = [
 			'article',
-			'book_review',
+			// 'book_review',
 			// .. add more node types here.
 		];
 
 		return array_filter( $node_types, fn( $type ) => in_array( $type, $types_to_migrate ), ARRAY_FILTER_USE_KEY );
+	}
+
+	public function fgd2wp_get_nodes_sql ( $sql, $prefix, $last_drupal_id, $limit, $content_type, $entity_type ) {
+			
+		// where clause.  the first time $last_drupal_id will be (int) 0 . this will cause the MAX ID to be on the top of DESC.
+		// upon each insert, $last_drupal_id will progressively get less.  so need to change to AND n.nid < '$last_drupal_id'
+		if( $last_drupal_id > 0 ) {
+			$sql = str_replace( 'AND n.nid > ', 'AND n.nid < ', $sql );
+		}
+
+		// order by.
+		$sql = str_replace( 'ORDER BY n.nid', 'ORDER BY n.nid DESC', $sql );
+
+		// limit for testing.
+		$sql = str_replace( 'LIMIT ' . $limit, 'LIMIT 1', $sql );
+
+		return $sql;
 	}
 
 	public function fgd2wp_map_taxonomy( $wp_taxonomy, $taxonomy ) {
@@ -109,13 +120,16 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 
 	public function fgd2wp_pre_register_post_type( $post_type, $node_type ) {
 		// Map to post.
-		if ( 'book_review' === $node_type ) {
-			$post_type = 'post';
-		}
+		// if ( 'book_review' === $node_type ) {
+		// 	$post_type = 'post';
+		// }
 
 		return $post_type;
 	}
 
+	/*
+	Replaced with premium option: 'only_authors' => true
+	// add_filter( 'fgd2wpp_get_users_sql',         [ $this, 'fgd2wpp_get_users_sql' ], 10, 2 );
 	public function fgd2wpp_get_users_sql( string $sql ): string {
 		
 		$limit        = 10; // Possibly used to "batch" x number at a time?
@@ -136,4 +150,6 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 
 		return $sql;
 	}
+	*/
+
 }
