94af95e4961fe7bf2d7ad9b119b5d203d2858192
Ivan Uravic <iuravic@gmail.com>
2025-01-22 14:27:36 +0100

    Bump up version number

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://newspack.com
  * Author:      Automattic
  * Author URI:  https://newspack.com
- * Version:     1.8.1
+ * Version:     1.8.2
  *
  * @package  Newspack_Custom_Content_Migrator
  */
