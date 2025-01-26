f9e31ed6b615b76ba763156e1bc3bde08e220b3f
ronchambers <ronchambers@gmail.com>
2025-01-26 09:48:29 -0800

    Add AmericaMag: clean up

--- src/Command/PublisherSpecific/AmericaMagMigrator.php
+++ src/Command/PublisherSpecific/AmericaMagMigrator.php
@@ -34,103 +34,35 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 		if ( ! is_plugin_active( "fg-drupal-to-wp-premium-entityreference-module/fg-drupal-to-wp-entityreference.php" ) ) {
 			NMT::exit_with_message( 'FG Drupal Entity Refernce Add-on plugin not found. Install and activate it before using this class.' );
 		}
-
+		
 		// Verify ET/-4 timezone:
+		// if we want to set this programatically (via wp_cli::confirm yes/no), the DB must match:
+		// timezone_string	America/New_York	auto
+		// gmt_offset		(null)				on|auto (?)
+		// just force this to be done by-hand in wp-admin instead.
 		if( wp_timezone_string() !== $this->required_timezone ) {
-			// if we want to set this programatically, the DB must match:
-			// timezone_string	America/New_York	auto
-			// gmt_offset		(null)				on|auto (?)
-			// just do this by hand in wp-admin instead.
-			WP_CLI::error( 'WP > settings > timezone must be set to: ' . $this->required_timezone );
+			// exit on error.
+			WP_CLI::error( 'WP > settings > timezone must be set to: ' . $this->required_timezone, true );
 		}
 
 		// Setup FG plugin's filters.
-		add_filter( 'fgd2wp_get_node_types',             [ $this, 'fgd2wp_get_node_types' ], 11, 1 );
-		add_filter( 'fgd2wp_get_nodes_sql',              [ $this, 'fgd2wp_get_nodes_sql' ], 10, 6 );
-		add_filter( 'fgd2wp_map_taxonomy',               [ $this, 'fgd2wp_map_taxonomy' ], 11, 3 );
-		add_filter( 'fgd2wp_post_import_post',           [ $this, 'fgd2wp_post_import_post' ], 10, 5 );
-		add_filter( 'fgd2wp_pre_register_post_type',     [ $this, 'fgd2wp_pre_register_post_type' ], 11, 3 );
+		add_filter( 'fgd2wp_get_node_types',                   [ $this, 'fgd2wp_get_node_types' ], 11, 1 );
+		add_filter( 'fgd2wp_get_nodes_sql',                    [ $this, 'fgd2wp_get_nodes_sql' ], 10, 6 );
+		add_filter( 'fgd2wp_map_taxonomy',                     [ $this, 'fgd2wp_map_taxonomy' ], 11, 3 );
+		add_filter( 'fgd2wp_post_import_post',                 [ $this, 'fgd2wp_post_import_post' ], 10, 5 );
+		add_action( 'fgd2wp_post_register_custom_post_fields', [ $this, 'fgd2wp_post_register_custom_post_fields' ], 10, 2 );
+		add_filter( 'fgd2wp_pre_insert_post',                  [ $this, 'fgd2wp_pre_insert_post' ], 10, 2 );
+		add_filter( 'fgd2wp_pre_register_post_type',           [ $this, 'fgd2wp_pre_register_post_type' ], 11, 3 );
 
-
-add_action( 'fgd2wp_post_register_custom_post_fields', function( $custom_fields, $post_type ) {
-	// Save drupal's post fields to memory incase we need to look them up later.
-	if( $post_type === 'post' ) $this->custom_post_fields = $custom_fields;
-}, 10, 2 );
-	
-add_filter( 'fgd2wp_pre_insert_post', function( $new_post, $node ) {
-	
-
-	if ( 'article' !== $node['type'] ) return;
-	
-	global $fgd2wpp;
-
-	// option 1: using an FG function.
-	if ( empty( $this->custom_post_fields['publication_date'] ) ) {
-		WP_CLI::error( 'Missing custom post field for: publication_date', true );
-	}
-	$result_array = $fgd2wpp->get_node_custom_field_values( $node, $this->custom_post_fields['publication_date'] );
-	// end option 1.
-
-	// option 2: via FG drupal query (sql).
-	// $sql = sprintf(
-	// 	"SELECT DISTINCT field_publication_date_value, f.delta
-	// 	FROM node__field_publication_date f
-	// 	WHERE f.entity_id = '%d'
-	// 	AND f.langcode IN('en', 'und')
-	// 	ORDER BY f.delta", 
-	// 	(int) $node['nid']
-	// );
-	// $result_array = $fgd2wpp->drupal_query( $sql, true ); // fail on db error.
-	// end option 2.
-
-	// Get value:
-	if ( 1 !== count( $result_array )
-		|| empty( $result_array[0]['field_publication_date_value'] )
-		|| false === strtotime( $result_array[0]['field_publication_date_value'])
-	) {
-		WP_CLI::error( 'Custom post field value is not a valid datetime for: publication_date', true );
-	}
-
-	// todo UTC/GMT vs normal date??
-	// ?? -0400 ?? $new_post['post_date'] = $result_array[0]['field_publication_date_value'];
-	// ?? $new_post['post_date_gmt'] = $result_array[0]['field_publication_date_value'];
-	
-	
-	// SET WORDPRESS DATE TO NEW YORK PRIOR TO IMPORT!!!!
-	// below will show correct -0400 in HTML
-	unset( $new_post['post_date'] ); // UNSET THIS SO IT WILL GET CREATED FROM GMT IN WP TIMEZONE (NEW YORK)
-	$new_post['post_date_gmt'] = $result_array[0]['field_publication_date_value']; // this alone will cause WP to create post_date -4/Easten Time.
-	// YES!!!  guid is created from post date so iF GTM is under 4 am, then GUID will be prior day - which is how DRUPAL does it!!
-	
-	WP_CLI::line( $result_array[0]['field_publication_date_value'] );
-
-	return $new_post;
-
-}, 10, 2 );
-
-
-
-		// Premium filters have extra "p" in name.s
+		// Premium filters have extra "p" in name.
 		add_filter( 'fgd2wpp_post_init_premium_options', [ $this, 'fgd2wpp_post_init_premium_options' ] );
 
-		// global $fgd2wpp;
-		// var_dump( $fgd2wpp );
-		// exit();
-		
-		// global $drupal_db;
-
-		// FYI: unset($wp_filter['wp_insert_post']); // Remove the "wp_insert_post" that consumes a lot of CPU and memory
-		
 		// Call NMT's migrator using a unique migration name.
 		DrupalMigrator::cmd_wrap_drupal_import( [ 'am_mag' ], [] );
 
 	}
 
-	/**
-	 * HOOKS
-	 */
-
-	 public function fgd2wp_get_node_types( array $node_types ): array {
+	public function fgd2wp_get_node_types( array $node_types ): array {
 		$types_to_migrate = [
 			'article',
 			// 'profile', // entity reference (FG plugin add-on required) | db.node__field_by_author
@@ -194,6 +126,11 @@ add_filter( 'fgd2wp_pre_insert_post', function( $new_post, $node ) {
 		) ) );
 	}
 
+	public function fgd2wp_post_register_custom_post_fields( $custom_fields, $post_type ) {
+		// Save drupal's custom post fields to memory so we can look them up later.
+		if( $post_type === 'post' ) $this->custom_post_fields = $custom_fields;
+	}
+
 	public function fgd2wp_pre_register_post_type( $post_type, $node_type ) {
 		// Map to post.
 		// if ( 'book_review' === $node_type ) {
@@ -239,6 +176,48 @@ add_filter( 'fgd2wp_pre_insert_post', function( $new_post, $node ) {
 		return $premium_options;
 	}
 
+	public function fgd2wp_pre_insert_post( $new_post, $node ) {
+	
+		if ( 'article' !== $node['type'] ) return $new_post;
+		
+		global $fgd2wpp;
+	
+		// option 1: using an FG function.
+		if ( empty( $this->custom_post_fields['publication_date'] ) ) {
+			WP_CLI::error( 'Missing custom post field for: publication_date', true );
+		}
+		$result_array = $fgd2wpp->get_node_custom_field_values( $node, $this->custom_post_fields['publication_date'] );
+		// end option 1.
+	
+		// option 2: via FG drupal query (sql).
+		// $sql = sprintf(
+		// 	"SELECT DISTINCT field_publication_date_value, f.delta
+		// 	FROM node__field_publication_date f
+		// 	WHERE f.entity_id = '%d'
+		// 	AND f.langcode IN('en', 'und')
+		// 	ORDER BY f.delta", 
+		// 	(int) $node['nid']
+		// );
+		// $result_array = $fgd2wpp->drupal_query( $sql, true ); // fail on db error.
+		// end option 2.
+	
+		// Get value:
+		if ( 1 !== count( $result_array )
+			|| empty( $result_array[0]['field_publication_date_value'] )
+			|| false === strtotime( $result_array[0]['field_publication_date_value'])
+		) {
+			WP_CLI::error( 'Custom post field value is not a valid datetime for: publication_date', true );
+		}
+	
+		// SET WORDPRESS DATE TO NEW YORK PRIOR TO IMPORT!!!!
+		// below will show correct -0400 in HTML
+		unset( $new_post['post_date'] ); // UNSET THIS SO IT WILL GET CREATED FROM GMT IN WP TIMEZONE (NEW YORK)
+		$new_post['post_date_gmt'] = $result_array[0]['field_publication_date_value']; // this alone will cause WP to create post_date -4/Easten Time.
+		// YES!!!  guid is created from post date so iF GTM is under 4 am, then GUID will be prior day - which is how DRUPAL does it!!
+	
+		return $new_post;	
+	}
+
 	/*
 	Replaced with premium option: 'only_authors' => true
 	// add_filter( 'fgd2wpp_get_users_sql',         [ $this, 'fgd2wpp_get_users_sql' ], 10, 2 );
@@ -263,5 +242,4 @@ add_filter( 'fgd2wp_pre_insert_post', function( $new_post, $node ) {
 		return $sql;
 	}
 	*/
-
 }
