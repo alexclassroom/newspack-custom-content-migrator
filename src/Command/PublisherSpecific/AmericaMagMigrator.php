8dc9ef01cb02063da572dd5bc0970043ffa79595
ronchambers <ronchambers@gmail.com>
2025-01-30 14:59:02 -0800

    Add AmericaMag: skip nodes and batching

--- src/Command/PublisherSpecific/AmericaMagMigrator.php
+++ src/Command/PublisherSpecific/AmericaMagMigrator.php
@@ -5,7 +5,6 @@ namespace NewspackCustomContentMigrator\Command\PublisherSpecific;
 use Newspack\MigrationTools\NMT;
 use Newspack\MigrationTools\Command\DrupalMigrator;
 use Newspack\MigrationTools\Command\WpCliCommandTrait;
-use Newspack\MigrationTools\Logic\DrupalHelper;
 use NewspackCustomContentMigrator\Command\RegisterCommandInterface;
 use WP_CLI;
 
@@ -13,11 +12,46 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 
 	use WpCliCommandTrait;
 
-	private string $migration_name = 'am_mag';
-	private string $required_timezone = 'America/New_York';
+	/**
+	 * Batch counts of imported nodes per type per CLI run.
+	 *
+	 * @var array
+	 */
+	private array $batch_counts;
 
+	/**
+	 * Batch max of imported nodes per type per CLI run.
+	 *
+	 * @var int
+	 */
+	private int $batch_max = 9;
+
+	/**
+	 * Custom Post Fields holds the related field definitions that are attached to node articles.
+	 *
+	 * @var array
+	 */
 	private array $custom_post_fields;
 
+	/**
+	 * Migration name, used as a unique identifier.
+	 *
+	 * @var string
+	 */
+	private string $migration_name = 'am_mag';
+
+	/**
+	 * Required timezone setting.
+	 *
+	 * @var string
+	 */
+	private string $required_timezone = 'America/New_York';
+
+	/**
+	 * CLI Commands
+	 *
+	 * @return void
+	 */
 	public static function register_commands(): void {
 		WP_CLI::add_command(
 			'newspack-content-migrator am-mag-import',
@@ -30,23 +64,22 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 	 */
 	public function cmd_run_import( array $pos_args, array $assoc_args ): void {
 		
-		// Verify the FG Entity add-on is active.
+		// Verify the FG Drupal "Entity Reference" add-on is active.
 		if ( ! is_plugin_active( "fg-drupal-to-wp-premium-entityreference-module/fg-drupal-to-wp-entityreference.php" ) ) {
 			NMT::exit_with_message( 'FG Drupal Entity Refernce Add-on plugin not found. Install and activate it before using this class.' );
 		}
 		
-		// Verify ET/-4 timezone:
-		// if we want to set this programatically (via wp_cli::confirm yes/no), the DB must match:
-		// timezone_string	America/New_York	auto
-		// gmt_offset		(null)				on|auto (?)
-		// just force this to be done by-hand in wp-admin instead.
+		// Verify America/New_York (eastern / utc-4 timezone):
 		if( wp_timezone_string() !== $this->required_timezone ) {
-			// exit on error.
+			// if we want to set this programatically (via wp_cli::confirm [yes/no]), the DB must match:
+			//   timezone_string	America/New_York	auto
+			//   gmt_offset		(null)				on|auto (?)
+			// so just force this to be done by-hand in wp-admin instead.  exit if not set.
 			WP_CLI::error( 'WP > settings > timezone must be set to: ' . $this->required_timezone, true );
 		}
 
 		// Setup FG plugin's filters.
-		add_filter( 'fgd2wp_get_node_types',                   [ $this, 'fgd2wp_get_node_types' ], 11, 1 );
+		// add_filter( 'fgd2wp_get_node_types',                [ $this, 'fgd2wp_get_node_types' ], 11, 1 );
 		add_filter( 'fgd2wp_get_nodes_sql',                    [ $this, 'fgd2wp_get_nodes_sql' ], 10, 6 );
 		add_filter( 'fgd2wp_map_taxonomy',                     [ $this, 'fgd2wp_map_taxonomy' ], 11, 3 );
 		add_filter( 'fgd2wp_post_import_post',                 [ $this, 'fgd2wp_post_import_post' ], 10, 5 );
@@ -54,46 +87,100 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 		add_filter( 'fgd2wp_pre_insert_post',                  [ $this, 'fgd2wp_pre_insert_post' ], 10, 2 );
 		add_filter( 'fgd2wp_pre_register_post_type',           [ $this, 'fgd2wp_pre_register_post_type' ], 11, 3 );
 
-		// Premium filters have extra "p" in name.
-		add_filter( 'fgd2wpp_post_init_premium_options', [ $this, 'fgd2wpp_post_init_premium_options' ] );
+		// Premium filters. Note the extra "p" in hook name.
+		add_filter( 'fgd2wpp_post_init_premium_options',       [ $this, 'fgd2wpp_post_init_premium_options' ] );
 
 		// Call NMT's migrator using a unique migration name.
-		DrupalMigrator::cmd_wrap_drupal_import( [ 'am_mag' ], [] );
-
+		DrupalMigrator::cmd_wrap_drupal_import( [ $this->migration_name ], [] );
 	}
 
+	/**
+	 * Batch method to get key for counts.
+	 *
+	 * @param string $content_type Node types: article, page, etc.
+	 * @param string $entity_type  Node, media, user, etc.
+	 * @return void
+	 */
+	private function batch_get_key( $content_type, $entity_type ) {
+		return $content_type . '---' . $entity_type;
+	}
+
+	/**
+	 * FG Drupal get nodes types.
+	 * 
+	 * Only use this filter to create an "allow list" for custom node types. By default, FG Drupal will
+	 * migrate all node types and uses the filter fgd2wpp_post_init_premium_options to set
+	 * $premium_options['nodes_to_skip'] if a certain node type needs to be skipped. It's preferred to 
+	 * skip nodes using $premium_options['nodes_to_skip']. But if that is too much management,
+	 * and instead you just want an "allow list", then use this filter.
+	 *
+	 * @param array $node_types
+	 * @return array
+	 */
 	public function fgd2wp_get_node_types( array $node_types ): array {
-		$types_to_migrate = [
-			'article',
-			// 'profile', // entity reference (FG plugin add-on required) | db.node__field_by_author
-			// 'book_review',
-			// .. add more node types here.
-		];
+		
+		return $node_types;
 
-		return array_filter( $node_types, fn( $type ) => in_array( $type, $types_to_migrate ), ARRAY_FILTER_USE_KEY );
+		// Always allow the following core node types in this filter. To remove these core node types
+		// from a migration they must be removed instead by using filter fgd2wpp_post_init_premium_options
+		// and adding the core node type to $premium_options['nodes_to_skip'] .
+		$allow = [ 'article', 'page', 'post', 'story' ];
+		
+		// Add custom node types to this list. Be advised that $premium_options['nodes_to_skip'] will take
+		// precedence over this list. To remove a custom node from a migration, do not add below, but instead
+		// add to filter fgd2wpp_post_init_premium_options / $premium_options['nodes_to_skip']
+		$allow[] = 'profile'; // entity reference (FG plugin add-on required) | database table: node__field_by_author
+		// @todo: $allow[] = 'book_review',
+		// @todo: .. add more custom node types here?
+
+		// Only return the node types that are in both $node_types and $allow.
+		return array_filter( $node_types, fn( $type ) => in_array( $type, $allow ), ARRAY_FILTER_USE_KEY );
 	}
 
-	public function fgd2wp_get_nodes_sql ( $sql, $prefix, $last_drupal_id, $limit, $content_type, $entity_type ) {
+	/**
+	 * FG Drupal get nodes sql.
+	 *
+	 * Use this filter to modify the sql query for the main import loop. When FG Drupal selects nodes to import
+	 * this is the SQL that it runs. The default sql will get 10 nodes in ascending node id order where the 
+	 * node ids are greater than the last previously imported node id. This sql will be run over-and-over again until
+	 * there are no more nodes remaining to import. 
+	 * 
+	 * @param [type] $sql            Default sql.
+	 * @param [type] $prefix         Database table prefix.
+	 * @param [type] $last_drupal_id Last imported node id. Initial value: 0
+	 * @param [type] $limit          Default limit of rows to select each batch: 10
+	 * @param [type] $content_type   Node content type.
+	 * @param [type] $entity_type    Node, media, user, taxonomy, etc.
+	 * @return void
+	 */
+	public function fgd2wp_get_nodes_sql( $sql, $prefix, $last_drupal_id, $limit, $content_type, $entity_type ) {
+		
+		// Use batching per CLI run.
+		$batch_key = $this->batch_get_key( $content_type, $entity_type );
+		if ( !isset( $this->batch_counts[ $batch_key ] ) ) {
+			$this->batch_counts[ $batch_key ] = 0;
+		}
+
+		// When importing articles, it's easier to test and QA the content when importing the newest articles
+		// first. The default sql will import the lower node ids first so this means the oldest articles will
+		// get imported before the newer articles. The following will change the SQL to import the newest
+		// content first.
+		if ( 'node' === $entity_type && 'article' == $content_type ) {
 			
-		if ( $content_type === 'article' ) {
+			// Order by nid desc to force newest content first.
+			$sql = str_replace( 'ORDER BY n.nid', 'ORDER BY n.nid DESC', $sql );
 
-			// where clause.  the first time $last_drupal_id will be (int) 0 . this will cause the MAX ID to be on the top of DESC.
-			// upon each insert, $last_drupal_id will progressively get less.  so need to change to AND n.nid < '$last_drupal_id'
-			// if( $last_drupal_id > 0 ) {
-				// for testing don't increment so we stay on 1 import; it will stick on the last id (which is the MAX value)
-				// $sql = str_replace( 'AND n.nid > ', 'AND n.nid < ', $sql );
-			// }
-			// order by.
-			// $sql = str_replace( 'ORDER BY n.nid', 'ORDER BY n.nid DESC', $sql );
-			// limit for testing.
-			// $sql = str_replace( 'LIMIT ' . $limit, 'LIMIT 1', $sql );
-
-			// hard coded
-			if( $last_drupal_id == 0 ) {
-				$sql = str_replace( "AND n.nid > '0'", "AND n.nid in(240610)", $sql );
-			} else {
-				$sql = str_replace( "AND n.nid > '" . $last_drupal_id . "'", "AND 1 = 2", $sql );
+			// Where ids are less than the last imported id since we're doing the newest (largest) ids first.
+			// But the first time this is called, the $last_drupal_id will be 0 so don't change the sql.
+			if ( $last_drupal_id > 0 ) {
+				$sql = str_replace( 'AND n.nid > ', 'AND n.nid < ', $sql );
 			}
+			
+			// Stop at batch limit.
+			if ( $this->batch_counts[ $batch_key ] >= $this->batch_max ) {
+				$sql = str_replace( 'LIMIT ' . $limit, 'LIMIT 0', $sql );
+			}
+
 		}
 		else if ( $content_type === 'profile' ) {
 			if( $last_drupal_id == 0 ) {
@@ -121,6 +208,10 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 	}
 
 	public function fgd2wp_post_import_post( $new_post_id, $node, $content_type, $post_type, $entity_type ) {
+
+		// Update batch count.
+		$this->batch_counts[ $this->batch_get_key( $content_type, $entity_type ) ]++;
+
 		WP_CLI::line( 'fgd2wp_post_import_post (AFTER): ' . json_encode( array( 
 			$new_post_id, $node, $content_type, $post_type, $entity_type,
 		) ) );
@@ -163,8 +254,39 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 	
 		$premium_options['only_authors'] = true;
 
-		// temp / testing:
-		$premium_options['nodes_to_skip']  = ['page'];
+		// By default FG drupal will migrate all core and custom node types.
+		// The core node types are 'article', 'page', 'post', 'story'
+		// To skip a core or custom node type add to the following array. 
+		// Note: if using fgd2wp_get_node_types it is possible to use that filter to skip
+		// custom nodes (but not core nodes).  
+		// to get node types with content: select distinct type from node order by type;
+		// to get node types from config:  select name from config where name like 'node.type.%' order by name;
+		$premium_options['nodes_to_skip']  = [ 
+			'america_special_topics',
+			'app_america_today_curated_articl',
+			'app_reels',
+			// allow: 'article',
+			'audio_news_update',
+			'audio_prayer',
+			'book',
+			'book_review',
+			'global_module_configuration',
+			'issue',
+			'lectionary_date',
+			'modular_page',
+			'page',
+			'photo_gallery',
+			'podcast',
+			'press_release',
+			// allow: 'profile',
+			'sponsorship',
+			'subscription_offer',
+			'the_word',
+			'video',
+			'webform_page',
+			'who_we_are_page',
+		];
+
 		$premium_options['skip_blocks']    = true; // sidebar widgets
 		$premium_options['skip_comments']  = true;
 		$premium_options['skip_menus']     = true;
@@ -178,6 +300,8 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 
 	public function fgd2wp_pre_insert_post( $new_post, $node ) {
 	
+		// return $new_post;
+
 		if ( 'article' !== $node['type'] ) return $new_post;
 		
 		global $fgd2wpp;
@@ -225,7 +349,7 @@ class AmericaMagMigrator implements RegisterCommandInterface {
 		
 		$limit        = 10; // Possibly used to "batch" x number at a time?
 		$last_user_id = (int) get_option( 'fgd2wp_last_user_id' ); // to restore the import where it left
-		$prefix       = DrupalHelper::get_tables_prefix();
+		$prefix       = \Newspack\MigrationTools\Logic\DrupalHelper::get_tables_prefix();
 
 		$sql = "
 			SELECT u.uid, u.name, u.mail, u.pass, u.created, up.user_picture_target_id AS picture
