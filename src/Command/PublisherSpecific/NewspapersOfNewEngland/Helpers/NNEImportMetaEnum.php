14056595963b62eba4c2aa3d0bb7479f89b8cc26
Eddie Carrasco <eddies.code.shop@gmail.com>
2025-07-10 13:44:48 -0400

    add: new import meta to record checksum-like filenames from XML export

--- src/Command/PublisherSpecific/NewspapersOfNewEngland/Helpers/NNEImportMetaEnum.php
+++ src/Command/PublisherSpecific/NewspapersOfNewEngland/Helpers/NNEImportMetaEnum.php
@@ -6,6 +6,7 @@ enum NNEImportMetaEnum: string {
 	case ARTICLE_ID_KEY               = 'newspack_legacy_article_id';
 	case IMAGE_DATA_ID_KEY            = 'newspack_legacy_image_data_id';
 	case IMAGE_EDITORIAL_ID_KEY       = 'newspack_legacy_image_editorial_id';
+	case IMAGE_CHECKSUM_KEY           = 'newspack_legacy_image_checksum_filename';
 	case IMAGE_DOC_NAME_KEY           = 'newspack_legacy_image_doc_name';
 	case IMAGE_INCLUDE_IN_GALLERY_KEY = 'newspack_legacy_image_include_in_gallery';
 	case IMAGE_INLINE_KEY             = 'newspack_legacy_image_inline';
